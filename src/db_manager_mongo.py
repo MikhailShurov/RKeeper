233958d395cd0a0bf42e64a58140c767b9b6b358
MikhailShurov <mixailshurov@gmail.com>
2024-09-27 16:42:31 +0300

    Added MongoDB, temporally unavailable docker

---
+++ src/db_manager_mongo.py
@@ -0,0 +1,57 @@
+import uuid
+
+from pymongo.database import Database
+from src.recipes.schemas import Recipe
+
+
+class DBManagerMongo:
+    def __init__(self, database: Database):
+        self.db = database
+
+    def init_user(self, user_id):
+        user = self.db.recipes.find_one({"_id": user_id})
+        if user is None:
+            self.db.recipes.insert_one({
+                "_id": user_id,
+                "recipes": []
+            })
+
+    async def user_get_recipes(self, user_id: str):
+        recipes = self.db.recipes.find({"_id": str(user_id)})
+        return list(recipes)
+
+    async def user_get_recipe(self, user_id, recipe_id: str):
+        # recipe = self.db.recipes.find_one({"_id": user})
+        pass
+
+    async def user_add_recipe(self, user_id: str, recipe: Recipe):
+        recipe_dict = recipe.model_dump()
+        recipe_dict["recipe_id"] = str(uuid.uuid4())
+        recipe_dict['cuisine'] = recipe_dict['cuisine'].value
+        self.init_user(user_id)
+        self.db.recipes.update_one(
+            {"_id": user_id},
+            {"$push": {"recipes": recipe_dict}}
+        )
+
+    async def user_delete_recipe(self, user_id: str, recipe_id: str):
+        self.init_user(user_id)
+        self.db.recipes.update_one(
+            {"_id": user_id},
+            {"$pull": {"recipes": {"recipe_id": recipe_id}}}
+        )
+
+    async def user_update_recipe(self):
+        pass
+
+    async def get_published(self):
+        pass
+
+    async def publish_recipe(self, recipe_id: str):
+        pass
+
+    async def unpublish_recipe(self, recipe_id: str):
+        pass
+
+    async def get_user_following_latest(self):
+        pass
---
+++ src/recipes/routers.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordBearer
+
+from src.recipes.schemas import Recipe, RecipeID
+from src.utils import decode_jwt_token, get_mongo_db_manager
+
+router = APIRouter()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+
+@router.get("/get_all")
+async def get_recipes(token: str = Depends(oauth2_scheme)):
+    token_payload = await decode_jwt_token(token)
+    user_id = token_payload["user_id"]
+    db_manager_mongo = await get_mongo_db_manager()
+    recipes = await db_manager_mongo.user_get_recipes(user_id)
+    return recipes
+
+
+# @router.get("/get_specific")
+# async def get_specific_recipe(recipe_id: RecipeID, token: str = Depends(oauth2_scheme)):
+#     token_payload = await decode_jwt_token(token)
+#     user_id = token_payload["user_id"]
+#     db_manager_mongo = await get_mongo_db_manager()
+#     recipes = await db_manager_mongo.user_get_recipes(user_id)
+
+
+@router.post("/add")
+async def add_recipe(recipe: Recipe, token: str = Depends(oauth2_scheme)):
+    token_payload = await decode_jwt_token(token)
+    user_id = token_payload["user_id"]
+    db_manager_mongo = await get_mongo_db_manager()
+    await db_manager_mongo.user_add_recipe(user_id, recipe)
+    return {"status": "recipe successfully added"}
+
+
+@router.post("/delete")
+async def delete_recipe(recipe_id: RecipeID, token: str = Depends(oauth2_scheme)):
+    token_payload = await decode_jwt_token(token)
+    recipe_id = str(recipe_id.model_dump()["recipe_id"])
+    user_id = token_payload["user_id"]
+    db_manager_mongo = await get_mongo_db_manager()
+    await db_manager_mongo.user_delete_recipe(user_id, recipe_id)
+    return {"status": "recipe successfully deleted"}
